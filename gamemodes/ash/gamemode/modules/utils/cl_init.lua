6d8fa0970ff521f70a7468711ea221fc40a95df5
Unknown Developer <olegis019@gmail.com>
2025-12-05 19:50:48 +0400

    Added `utils.loadMaterial`

--- gamemodes/ash/gamemode/modules/utils/cl_init.lua
+++ gamemodes/ash/gamemode/modules/utils/cl_init.lua
@@ -1,5 +1,137 @@
+---@type dreamwork
+local dreamwork = _G.dreamwork
+
+---@type dreamwork.std
+local std = dreamwork.std
+
+local math = std.math
+local math_isint = math.isint
+
+local raw = std.raw
+local raw_pairs = raw.pairs
+
+local string = std.string
+local string_lower = string.lower
+local string_isURL = string.isURL
+
+local fs = std.fs
+fs.makeDirectory( "/garrysmod/data/ash/downloads/images", true )
+
 ---@class ash.utils
 local utils = include( "shared.lua" )
+
+do
+
+    local engine_loadMaterial = dreamwork.engine.loadMaterial
+    local CreateMaterial = CreateMaterial
+    local type = std.type
+
+    local Material = Material
+    local Material_SetInt = Material.SetInt
+    local Material_SetFloat = Material.SetFloat
+    local Material_GetKeyValues = Material.GetKeyValues
+
+    local type2fn = {
+        string = Material.SetString,
+        ---@param material IMaterial
+        ---@param key string
+        ---@param value number
+        number = function( material, key, value )
+            if math_isint( value ) then
+                Material_SetInt( material, key, value )
+            else
+                Material_SetFloat( material, key, value )
+            end
+        end,
+        ITexture = Material.SetTexture,
+        VMatrix = Material.SetMatrix,
+        Vector = Material.SetVector,
+    }
+
+    ---@param from IMaterial
+    ---@param to IMaterial
+    local function translate( from, to )
+        for key, value in raw_pairs( Material_GetKeyValues( from ) ) do
+            local fn = type2fn[ type( value ) ]
+            if fn ~= nil then
+               fn( to, key, value )
+            end
+        end
+    end
+
+    ---@type table<string, boolean>
+    local supported_formats = {
+        jpeg = true,
+        png = true
+    }
+
+    --- [SHARED]
+    ---
+    --- Load a material from the specified path.
+    ---
+    ---@param name string
+    ---@param path string
+    ---@param image_parameters? dreamwork.engine.ImageParameters
+    ---@param shader_parameters? dreamwork.std.Material.Shader
+    function utils.loadMaterial( name, path, image_parameters, shader_parameters )
+        if shader_parameters == nil then
+            shader_parameters = {}
+        end
+
+        local shader = shader_parameters.name or "UnlitGeneric"
+        shader_parameters.name = nil
+
+        shader_parameters["$basetexture"] = shader_parameters["$basetexture"] or ash.LoadingTextureName or "color/white"
+        shader_parameters["$translucent"] = shader_parameters["$translucent"] or 1
+        shader_parameters["$vertexalpha"] = shader_parameters["$vertexalpha"] or 1
+        shader_parameters["$vertexcolor"] = shader_parameters["$vertexcolor"] or 1
+
+        local material = CreateMaterial( name, shader, shader_parameters )
+
+        if string_isURL( path ) then
+            http.Fetch( path, function( body, _, headers, code )
+                if code ~= 200 then return end
+
+                for key, value in raw_pairs( headers ) do
+                    headers[ key ] = nil
+                    headers[ string_lower( key ) ] = value
+                end
+
+                local content_type = headers["content-type"]
+                if content_type == nil then
+                    error( "failed to fetch data from URL (" .. path .. ") - no content-type" )
+                end
+
+                local content, format = string.match( content_type, "^([^/]+)/([^/;]+)" )
+                if content == nil or format == nil then
+                    error( "failed to fetch data from URL (" .. path .. ") - invalid content-type" )
+                end
+
+                if content ~= "image" then
+                    error( "failed to fetch data from URL (" .. path .. ") - not an image" )
+                end
+
+                if supported_formats[ format ] == nil then
+                    error( "failed to fetch data from URL (" .. path .. ") - unsupported format ( " .. format .. " )" )
+                end
+
+                local file_name = std.checksum.adler32( body ) .. "." .. format
+                local file_path = "ash/downloads/images/" .. file_name
+
+                if not file.Exists( file_path, "DATA" ) then
+                    file.Write( "ash/downloads/images/" .. file_name, body )
+                end
+
+                translate( engine_loadMaterial( "data/" .. file_path, image_parameters ), material )
+            end )
+        else
+            translate( engine_loadMaterial( path, image_parameters ), material )
+        end
+
+        return material
+    end
+
+end
 
 --- [SHARED]
 ---
