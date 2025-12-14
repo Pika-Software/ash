9a86f1a73308923c1f976f5d8e5e862590131f5d
Unknown Developer <olegis019@gmail.com>
2025-12-15 00:12:06 +0400

    voice lib

---
+++ gamemodes/ash/gamemode/autorun/player/voice/cl_init.lua
@@ -0,0 +1,69 @@
+local Player_GetVoiceVolumeScale = Player.GetVoiceVolumeScale
+local Player_SetVoiceVolumeScale = Player.SetVoiceVolumeScale
+local Player_IsVoiceAudible = Player.IsVoiceAudible
+local Player_VoiceVolume = Player.VoiceVolume
+
+local Entity_GetNW2Float = Entity.GetNW2Float
+local Entity_SetNW2Float = Entity.SetNW2Float
+
+local hook_Run = hook.Run
+
+---@type ash.player
+local ash_player = require( "ash.player" )
+
+---@class ash.player.voice
+local ash_voice = {}
+
+--- [CLIENT]
+---
+--- Checks if the player's voice is audible.
+---
+---@param pl Player
+function ash_voice.isAudible( pl )
+    return hook_Run( "PlayerCanHearPlayersVoice", ash_player.Entity, pl ) or Player_IsVoiceAudible( pl )
+end
+
+--- [CLIENT]
+---
+--- Gets the player's voice volume.
+---
+---@return number volume The player's voice volume. Range is from 0 to 1.
+function ash_voice.getVolume( pl )
+    local volume = Player_VoiceVolume( pl )
+    return hook_Run( "PlayerVoiceVolume", pl, volume ) or volume
+end
+
+--- [CLIENT]
+---
+--- Gets the player's voice volume scale.
+---
+---@return number scale The player's voice volume scale. Range is from 0 to 1.
+local function getVolumeScale( pl )
+    local scale = Entity_GetNW2Float( pl, "m_fVoiceVolumeScale", 1 )
+    return hook_Run( "PlayerVoiceVolumeScale", pl, scale ) or scale
+end
+
+ash_voice.getVolumeScale = getVolumeScale
+
+--- [CLIENT]
+---
+--- Sets the player's voice volume scale.
+---
+---@param pl Player
+---@param scale number
+---@diagnostic disable-next-line: duplicate-set-field
+function ash_voice.setVolumeScale( pl, scale )
+    Entity_SetNW2Float( pl, "m_fVoiceVolumeScale", scale )
+end
+
+hook.Add( "PlayerThink", "VolumeController", function( pl, me, is_local )
+    if is_local then return end
+
+    local scale = getVolumeScale( pl )
+
+    if Player_GetVoiceVolumeScale( pl ) ~= scale then
+        Player_SetVoiceVolumeScale( pl, scale )
+    end
+end, PRE_HOOK )
+
+return ash_voice
