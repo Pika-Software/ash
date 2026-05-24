7b6f2824265936e54b2cb775544406c4b8c3fd7c
denomito1 <denomito@protonmail.com>
2026-05-24 19:57:25 +0700

    add new entity `ash_trigger`

---
+++ gamemodes/ash/gamemode/entities/ash_trigger/init.lua
@@ -0,0 +1,73 @@
+---@class ash.trigger : ENT
+---@field EntityList table
+---@field EntityMap table
+local ENT = ENT
+
+ENT.Type = "brush"
+ENT.Base = "base_brush"
+
+function ENT:Initialize()
+    self:SetTrigger( true )
+    self:SetSolid( SOLID_BBOX )
+
+    self.EntityList = { [0] = 0 }
+    self.EntityMap = {}
+end
+
+---@param entity Entity
+function ENT:startTouch( entity ) end
+
+---@param entity Entity
+function ENT:endTouch( entity ) end
+
+---@param entity Entity
+function ENT:touch( entity ) end
+
+--- [SERVER]
+---
+--- Checking touching trigger zone.
+---
+---@param entity Entity
+---@return boolean
+function ENT:inZone( entity )
+    if self.EntityMap[ entity ] then
+        return true
+    end
+
+    return false
+end
+
+function ENT:Touch( entity )
+    local entity_map = self.EntityMap
+    local entity_list = self.EntityList
+    if not entity_map[ entity ] then
+        entity_map[ entity ] = true
+
+        local new_count = entity_list[ 0 ] + 1
+        entity_list[ 0 ] = new_count
+
+        entity_list[ new_count ] = entity
+
+        self:startTouch( entity )
+    end
+
+    self:touch( entity )
+end
+
+do
+    local table_removeByValue = table.removeByValue
+
+    function ENT:EndTouch( entity )
+        local entity_map = self.EntityMap
+        local entity_list = self.EntityList
+
+        entity_map[ entity ] = nil
+
+        self:endTouch( entity )
+
+        if table_removeByValue( entity_list, entity, entity_list[ 0 ] ) ~= nil then
+            local new_count = entity_list[ 0 ] - 1
+            entity_list[ 0 ] = new_count
+        end
+    end
+end
