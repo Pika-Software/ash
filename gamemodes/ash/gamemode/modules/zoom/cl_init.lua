a4da080a0666601d25f3c98d4779a9365da23d2f
Unknown Developer <olegis019@gmail.com>
2026-06-30 21:18:37 +0400

    refactor: some improvements in zoom module

--- gamemodes/ash/gamemode/modules/zoom/cl_init.lua
+++ gamemodes/ash/gamemode/modules/zoom/cl_init.lua
@@ -1,12 +1,10 @@
 -- TODO: maybe add convars to configure the parameters
 
+---@type ash.player
+local ash_player = import "ash.player"
 
-local hook_Add = hook.Add
-local math_min = math.min
-local math_max = math.max
-local Lerp = Lerp
+local math_clamp = math.clamp
 local FrameTime = FrameTime
-local LocalPlayer = LocalPlayer
 
 local hook_name = "ash.zoom"
 --- @class ash.zoom
@@ -26,6 +24,7 @@ local factor = nil
 --- [CLIENT]
 ---
 --- Enables zoom functionality on `+zoom`
+---
 --- @param config ash.zoom.config
 function ash_zoom.setup( config )
     local variable = config.variable or false
@@ -33,11 +32,13 @@ function ash_zoom.setup( config )
     local factor_max = config.factor_max or 1.0
     local factor_step = config.factor_step or 0.025
     local default_factor = config.default_factor or 0.5
+
     factor = default_factor
+
     local speed = 14
 
-    hook_Add( "ash.player.Key", hook_name, function( ply, key, pressed )
-        if ply ~= LocalPlayer() then return end
+    hook.Add( "ash.player.Key", hook_name, function( pl, key, pressed )
+        if not ash_player.isLocal( pl ) then return end
 
         if key ~= IN_ZOOM then return end
 
@@ -47,61 +48,60 @@ function ash_zoom.setup( config )
             is_zooming = false
             factor = default_factor
         end
-    end, NORMAL_HOOK )
+    end )
 
-    hook_Add( "PlayerBindPress", hook_name .. ".toggle", function( ply, bind )
-        if ply ~= LocalPlayer() then return end
+    hook.Add( "PlayerBindPress", hook_name .. ".toggle", function( pl, bind )
+        if not ash_player.isLocal( pl ) then return end
 
         if bind == "toggle_zoom" then
             is_zooming = not is_zooming
         end
-    end, NORMAL_HOOK )
+    end )
 
     if variable then
-        hook_Add( "ash.player.MouseWheel", hook_name, function( ply, wheel )
-            if ply ~= LocalPlayer() then return end
+
+        hook.Add( "ash.player.MouseWheel", hook_name, function( pl, wheel )
+            if not ash_player.isLocal( pl ) then return end
 
             if not is_zooming then return end
 
-            if wheel > 0 then
-                factor = math_max( factor_min, factor - factor_step )
-            else
-                factor = math_min( factor_max, factor + factor_step )
-            end
-        end, NORMAL_HOOK )
+            factor = math_clamp( factor - (factor_step * wheel), factor_min, factor_max )
+        end )
 
-        hook_Add( "PlayerBindPress", hook_name, function( ply, bind )
-            if ply ~= LocalPlayer() then return end
+        hook.Add( "PlayerBindPress", hook_name, function( pl, bind )
+            if not ash_player.isLocal( pl ) then return end
 
             if is_zooming and (bind == "invprev" or bind == "invnext") then
                 return true
             end
-        end, NORMAL_HOOK )
+        end )
     end
 
-    hook_Add( "CalcView", hook_name, function( ply, _, _, fov )
+    hook.Add( "CalcView", hook_name, function( ply, _, _, fov )
         if not ply:Alive() then return end
 
-        local target_zoom = is_zooming and fov * factor or fov
-        if tr_zoom == nil then tr_zoom = fov end
+        local target_zoom = is_zooming and (fov * factor) or fov
 
-        tr_zoom = Lerp( speed * FrameTime(), tr_zoom, target_zoom )
+        if tr_zoom == nil then
+            tr_zoom = fov
+        end
+
+        tr_zoom = math.lerp( speed * FrameTime(), tr_zoom, target_zoom )
 
         return { fov = tr_zoom }
-    end, NORMAL_HOOK )
+    end )
 end
-
-local hook_Remove = hook.Remove
 
 --- [CLIENT]
 ---
 --- Disable all existing zoom functionality.
 --- Removes hooks associated with the zoom module.
+---
 function ash_zoom.teardown()
-    hook_Remove( "ash.player.Key", hook_name )
-    hook_Remove( "ash.player.MouseWheel", hook_name )
-    hook_Remove( "PlayerBindPress", hook_name )
-    hook_Remove( "CalcView", hook_name )
+    hook.Remove( "ash.player.Key", hook_name )
+    hook.Remove( "ash.player.MouseWheel", hook_name )
+    hook.Remove( "PlayerBindPress", hook_name )
+    hook.Remove( "CalcView", hook_name )
 end
 
 return ash_zoom
